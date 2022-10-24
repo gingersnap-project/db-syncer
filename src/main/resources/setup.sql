e1cd27bc28d6a468e8fd8f063d12bc3e8926760d
William Burns <mudokonman@gmail.com>
2022-10-24 15:36:54 -0700

    Steps to start mysql running in docker for a very easily repeatable run

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -1,3 +1,4 @@
 create schema debezium;
 create table debezium.customer(id int not null, fullname varchar(255), email varchar(255));
 create table debezium.car_model(id int not null, model varchar(255), brand varchar(255));
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'gingersnap_user'
