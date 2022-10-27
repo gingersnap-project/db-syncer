de987cb87d06eedf1f09e365a917ce1f754fd9af
William Burns <mudokonman@gmail.com>
2022-10-27 09:22:04 -0700

    Disable dev services and IT test for now to prevent running extra servers with `quarkus dev`

--- src/test/java/org/gingesnap/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.gingesnap;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
