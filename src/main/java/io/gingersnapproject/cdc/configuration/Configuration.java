1fee126359aa40f027f9892137a578b231f19550
jabolina <jbolina@redhat.com>
2023-01-19 09:39:07 -0300

    Adding a simple default DNS query resolver
    * Converts the DNS to a list of addresses.
    
    * Polling DNS for new members

--- src/main/java/io/gingersnapproject/cdc/configuration/Configuration.java
+++ src/main/java/io/gingersnapproject/cdc/configuration/Configuration.java
@@ -3,10 +3,14 @@ package io.gingersnapproject.cdc.configuration;
 import java.util.Map;
 
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
 @ConfigMapping(prefix = "gingersnap")
 public interface Configuration {
+
+   @WithDefault("false")
+   boolean dynamicMembership();
 
    Database database();
 
