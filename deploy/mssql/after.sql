6a254fda58c8d295e72705bdd1bf2c090fe7c720
Ryan Emerson <remerson@redhat.com>
2023-02-17 10:27:00 +0000

    Simplify MSSQL deployment

--- deploy/mssql/after.sql
+++
@@ -1,9 +0,0 @@
-use debezium
-go
-
-EXEC sys.sp_cdc_enable_db;
-go
-
-EXEC sys.sp_cdc_enable_table @source_schema = N'debezium', @source_name = N'customer', @role_name = NULL, @supports_net_changes = 0
-go
-
